3595f59d7c237a44a41b294f36f857f19e425e9e
Phil Gran <philgran@gmail.com>
2021-02-10 15:30:30 -0600

    Adding support for hostname and ASIN replacement in URL, setting android deeplinks on click instead of page load

--- public/utm-replace.js
+++ public/utm-replace.js
@@ -1,5 +1,20 @@
 // Put this immediately before the closing </body> tag
 (function(){
+  function getMobileOperatingSystem () {
+    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    // Windows Phone must come first because its UA also contains "Android"
+    switch(true) {
+      case /windows phone/i.test(userAgent):
+        return "Windows Phone";
+      case /android/i.test(userAgent):
+        return "Android";
+      case /iPad|iPhone|iPod/.test(userAgent) && !window.MSStream:
+        return "iOS";
+      default:
+        return "unknown";
+    }
+}
+
   var term = new URLSearchParams(window.location.search).get('utm_term'); // Get our utm_term from the URL search params
   if (!term) return; // Abort if our URL doesn't have a utm_term
 
@@ -15,16 +30,20 @@
     if (linkParams.get('tag')) {
       linkParams.set('tag', term);
       url.search = linkParams.toString();
+       // Replace existing link href with updated url containing new params
+      link.href = url.toString();
     } else {
       return; // Abort if amazon link has no tag param
     }
-    var ua = new UAParser();
-    // if uastring includes android, change to android intent link
-    if (ua.getOS().name.toLowerCase() === 'android') {
-      // TODO: this URL is brittle, we should probably make it more dynamic pulling the parts we need from other amazon URLs on the page
-      url = new URL(`intent://#Intent;package=com.amazon.mShop.android.shopping;scheme=com.amazon.mobile.shopping.web://amazon.com/o/ASIN/B075K7W3BB/${term}/ref=nosim//;end`);
+    // if uastring includes android, change to android intent link and bind a click handler
+    if (getMobileOperatingSystem() === 'Android') {
+      const ASIN = url.pathname.match("(?:[/dp/]|$)([A-Z0-9]{10})")[0].replace('/', '');
+      // Build new Android deep link url with params from original URL
+      url = new URL(`intent://#Intent;package=com.amazon.mShop.android.shopping;scheme=com.amazon.mobile.shopping.web://${url.hostname}/o/ASIN/${ASIN}/${term}/ref=nosim//;end`);
+      link.addEventListener('click', function() {
+        // When the link is clicked, set the window location to our Android deep link
+        window.location.href = url;
+      });
     }
-    // Replace existing link href with updated url containing new params
-    link.href = url.toString();
   });
 })();
